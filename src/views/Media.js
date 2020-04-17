deae636361f4b105cf8e14d889f3d17669c8cb5a
jennaturunen <jenna.turunen2@gmail.com>
2020-04-17 10:44:18 +0300

    backbtn

--- src/views/Media.js
+++ src/views/Media.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MediaTable from '../components/MediaTable';
+import { Link as RouterLink } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 
 const Media = () => {
   return (
     <div>
       <h1> Media nappula kumpi row haetaan </h1>
       <MediaTable />
-      <button>uploadiin</button>
+      <Button component={RouterLink} to='/upload'>
+        Uploadiin
+      </Button>
     </div>
   );
 };
