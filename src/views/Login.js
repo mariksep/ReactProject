a74f6aa21f7dd6267d60c220b2eebd253b3adaca
jennaturunen <jenna.turunen2@gmail.com>
2020-05-02 14:32:40 +0300

    typo from loginpage

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,85 +1,73 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import RegisterForm from '../components/RegisterForm';
 import LoginForm from '../components/LoginForm';
-import {Button, Typography, FormGroup, Switch} from '@material-ui/core/';
+import { Button, Typography, FormGroup, Switch } from '@material-ui/core/';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import {Grid} from '@material-ui/core/';
-import {makeStyles} from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
     width: '100vw',
     height: '100vh',
-
   },
   text: {
-    'margin': '1em',
-    'textAlign': 'center',
-
+    margin: '1em',
+    textAlign: 'center',
   },
-
 });
-
 
 const Login = () => {
   const classes = useStyles();
-  const [show, setShow]= useState(true);
-  const showForms = () =>{
+  const [show, setShow] = useState(true);
+  const showForms = () => {
     setShow(!show);
   };
 
   return (
     <>
-
       <Grid
         container
-        direction="column"
-        alignItems="center"
+        direction='column'
+        alignItems='center'
         justify='center'
         className={classes.root}
       >
         <Grid
-
-          direction="column"
-          alignItems="center"
+          direction='column'
+          alignItems='center'
           justify='center'
           className={classes.text}
         >
-          <Typography
-            disableTypography={true}
-            variant='h1'>NEIGHBOR HELP
+          <Typography disableTypography={true} variant='h1'>
+            NEIGHBOR HELP
           </Typography>
-          <Typography variant='subtitle1' >
-          Welcome to the neighbor help app. The neighbor help app helps you
-          find help for everyday tasks and you can also offer it to others
-          who need help </Typography>
+          <Typography variant='subtitle1'>
+            Welcome to the neighbor help app. The neighbor help app helps you
+            find help for everyday tasks and you can also offer it to others who
+            need help{' '}
+          </Typography>
         </Grid>
-        <Grid
-
-          direction="column"
-          alignItems="center"
-          justify='center'
-        >
-
+        <Grid direction='column' alignItems='center' justify='center'>
           <FormGroup>
             <Typography component='div'>
-              <Grid component='label' container justify='center' alignItems='center' spacing={1}>
+              <Grid
+                component='label'
+                container
+                justify='center'
+                alignItems='center'
+                spacing={1}
+              >
                 <Grid item> Login</Grid>
-                <Switch color="default" onChange={showForms}
-                  name="checkedA" />
-                <Grid item> Ragister</Grid>
+                <Switch color='default' onChange={showForms} name='checkedA' />
+                <Grid item> Register</Grid>
               </Grid>
             </Typography>
-
           </FormGroup>
 
-          {show ?
-        <LoginForm/> :
-        <RegisterForm/>
-          }
+          {show ? <LoginForm /> : <RegisterForm />}
         </Grid>
       </Grid>
-
     </>
   );
 };
